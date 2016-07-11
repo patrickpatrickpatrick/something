16319b2a216333bd605e785cf16c29533e34e848
patrick <s1228955@sms.ed.ac.uk>
2016-07-11 05:56:11 +0100

    so it kinda works...need to fix users

--- app/controllers/conversations_controller.rb
+++ app/controllers/conversations_controller.rb
@@ -5,14 +5,23 @@ class ConversationsController < ApplicationController
   end
 
   def create
-  	recipients = User.where(id: conversation_params[:recipients])
-  	conversation = current_user.send_message(recipients, conversation_params[:body], conversation_params[:subject]).conversation
-    flash[:success] = "Your message was successfully sent!"
-    redirect_to conversation_path(conversation)
+    @opponent = User.find(conversation_params[:recipients].last)
+    conv_check_1 = Mailboxer::Conversation.participant(@opponent)
+    conv_check_2 = Mailboxer::Conversation.participant(current_user)
+    @dialog = (conv_check_1 & conv_check_2).first
+    if @dialog.nil? or !@dialog.is_participant?(current_user)
+       conversation = current_user.send_message(@opponent, conversation_params[:body], conversation_params[:subject]).conversation
+       flash[:success] = "Your message was successfully sent!"
+       redirect_to conversation_path(conversation)
+    else
+      current_user.reply_to_conversation(@dialog, conversation_params[:body])
+      flash[:notice] = "Your reply message was successfully sent!"
+      redirect_to conversation_path(@dialog)
+    end
   end
 
   def show
-	@receipts = conversation.receipts_for(current_user)
+	  @receipts = conversation.receipts_for(current_user)
     conversation.mark_as_read(current_user)
   end
 
